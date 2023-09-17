39cb7f5e6e3c1f8be21049b01b94d34deaba32a3
pustart <polaris9955@gmail.com>
2023-09-18 01:34:21 +0300

    feat: add autoscrolling

--- src/components/core/Button/Button.jsx
+++ src/components/core/Button/Button.jsx
@@ -1,9 +1,11 @@
 import clsx from 'clsx';
 import styles from './Button.module.scss';
 
-function Button({type = 'button', icon = null, children, className, onClick}) {
+function Button({type = 'button', icon = null, children, className, onClick, view}) {
   return (
-    <button type={type} onClick={onClick} className={clsx(styles.button, className)}>
+    <button type={type} onClick={onClick} className={clsx(styles.button, className, {
+      [styles['button__default']]: view === 'default',
+    })}>
       {icon}
       {children}
     </button>
